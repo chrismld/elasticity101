3834cc6d5d41d31d755c3a15c0965067ecf1c89b
Christian Melendez <christianhxc@gmail.com>
2020-06-30 16:08:54 +0200

    Added a new test case to confirm another scenario

--- src/app_test.go
+++ src/app_test.go
@@ -23,3 +23,21 @@ func TestApplicationHandler(t *testing.T) {
 			rr.Body.String(), expected)
 	}
 }
+
+func TestApplicationHandlerCustom(t *testing.T) {
+	req, err := http.NewRequest("GET", "/hello", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(ApplicationHandler)
+
+	handler.ServeHTTP(rr, req)
+
+	expected := "hello"
+	if rr.Body.String() != expected {
+		t.Errorf("Sorry, the ApplicationHandler is not working, got response %v and should have %v",
+			rr.Body.String(), expected)
+	}
+}
