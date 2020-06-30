e6eb346d74e802b035c92e7453bafe2153b73345
Christian Melendez <christianhxc@gmail.com>
2020-06-30 16:00:22 +0200

    Added the initial test for the app

---
+++ src/app_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestApplicationHandler(t *testing.T) {
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(ApplicationHandler)
+
+	handler.ServeHTTP(rr, req)
+
+	expected := ""
+	if rr.Body.String() != expected {
+		t.Errorf("Sorry, the ApplicationHandler is not working, got response %v and should have %v",
+			rr.Body.String(), expected)
+	}
+}
